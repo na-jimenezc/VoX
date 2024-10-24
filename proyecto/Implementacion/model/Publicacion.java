eb7176f4165508639fa32c5a69977ec5791bcfb8
Katheryn Guasca <katheryng2017@gmail.com>
2024-10-24 23:30:29 +0000

    Agregando prototipos/firmas de los metodos de la clase Publicacion  sin implementar aun

--- proyecto/Implementacion/model/Publicacion.java
+++ proyecto/Implementacion/model/Publicacion.java
@@ -1,3 +1,42 @@
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;   
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter   
+
+@ToString
 public class Publicacion {
 
+    @Id
+    @GeneratedValue
+    private Long idPub;
+
+    private String descripcion;
+    private Date fecha;
+    private Boolean anonimo;
+
+    // Constructor con parámetros para los atributos principales
+    public Publicacion(Long idAutor, String descripcion, Boolean anonimo) {
+        this.descripcion = descripcion;
+        this.fecha = new Date(); // Fecha de creación actual
+        this.anonimo = anonimo;
+    }
+
+    // Método para cambiar el anonimato de una publicación
+    public void cambiarAnonimato(Long idPub, Boolean anonimo) {
+        this.anonimo = anonimo;
+    }
 }
