45c4db60a1d1cba796011fe1125ebb186c6c3216
Katheryn Guasca <katheryng2017@gmail.com>
2024-10-24 22:58:39 +0000

    Modificando dependencias de Lombok para la entidad Usuario funcione correctamente

--- proyecto/Implementacion/model/Usuario.java
+++ proyecto/Implementacion/model/Usuario.java
@@ -1,3 +1,48 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AccessLevel; 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString
 public class Usuario {
 
+    @Id
+    @GeneratedValue
+    Long idUsuario;
+
+    String nombre;
+    String username;
+    String password;
+    int edad;
+    String carrera;
+    String semestre;
+    String biografia;
+    String email;
+  //  List<Usuario> siguiendo = new ArrayList<>();
+
+
+  //Metodos de la clase
+    public void seguir(Usuario usuario) {
+        // Implement logic to add user to following list (consider using JPA relationships)
+    }
+
+    public void dejarSeguir(Usuario usuario) {
+        // Implement logic to remove user from following list
+    }
 }
