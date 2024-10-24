4b40b947916ce11ab81e5664ed406b8597956e4a
Katheryn Guasca <katheryng2017@gmail.com>
2024-10-24 23:21:03 +0000

    Agregando prototipos/firmas de los metodos de la clase Like sin implementar aun

--- proyecto/Implementacion/model/Like.java
+++ proyecto/Implementacion/model/Like.java
@@ -1,3 +1,41 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@ToString
 public class Like {
+
+    @Id
+    @GeneratedValue
+    private Long idLike;
+
+    private Long idUser;
+    private Long idPub;
+    private Boolean anonimoLike;
+
+    // Constructor con parámetros para los atributos principales
+    public Like(Long idUser, Long idPub, Boolean anonimo) {
+        this.idUser = idUser;
+        this.idPub = idPub;
+        this.anonimoLike = anonimo;
+    }
+
+    //Metodos de la clase
+    public void eliminarLike(Like like) {
+
+    }
 
 }
