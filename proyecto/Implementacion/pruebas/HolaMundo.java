6cd7cfc7826badd7700021d2aff81ac4741061f7
Katheryn Guasca <katheryng2017@gmail.com>
2024-10-24 22:44:20 +0000

    Implementando modelo MVC en el proyecto para la creación de entidades usando JPA

---
+++ proyecto/Implementacion/pruebas/HolaMundo.java
@@ -0,0 +1,12 @@
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    public class HolaMundo {
+    
+        @GetMapping("/")
+        public String getMessage()  {
+            return "Hola Mundo !!";
+        }
+
+    }
