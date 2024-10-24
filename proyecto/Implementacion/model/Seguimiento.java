6ac61560befe4169099ac1d8d0b0a8b8c006b0c9
Katheryn Guasca <katheryng2017@gmail.com>
2024-10-24 23:35:49 +0000

    Agregando prototipos/firmas de los metodos de la clase Seguimiento  sin implementar aun

--- proyecto/Implementacion/model/Seguimiento.java
+++ proyecto/Implementacion/model/Seguimiento.java
@@ -1,3 +1,44 @@
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter 
+
+@ToString
 public class Seguimiento {
 
+    @Id
+    @GeneratedValue
+    private Long idSeguimiento;
+
+    private Long idSeguido;
+    private Long idSeguidor;
+
+    // Constructor con parámetros para los atributos principales
+    public Seguimiento(Long idSeguido, Long idSeguidor) {
+        this.idSeguido = idSeguido;
+        this.idSeguidor = idSeguidor;
+    }
+
+        // Métodos de la clase 
+    public void eliminarSeguimiento() {
+
+    }
+
+    public List<Usuario> obtenerSeguidoresUsuario(Long idUsuario) {
+
+    }
 }
