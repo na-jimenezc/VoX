31604944178fea1b5162d0d0cac14b37bfb83faf
Katheryn Guasca <katheryng2017@gmail.com>
2024-10-27 19:09:54 +0000

    Agregando Interfaz LikeRepository

---
+++ proyecto/src/main/java/Repository/LikeRepository.java
@@ -0,0 +1,11 @@
+package Repository;
+
+import Modelo.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    // Metodos
+}
+
