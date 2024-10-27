1888f2aecef70df7f5f95ccb3ab48e7747e17c0f
Katheryn Guasca <katheryng2017@gmail.com>
2024-10-27 18:31:48 +0000

    Agregando metodos de la clase Publicacion

--- proyecto/Implementacion/model/Publicacion.java
+++ proyecto/Implementacion/model/Publicacion.java
@@ -1,21 +1,22 @@
+import java.util.ArrayList;
 import java.util.Date;
-
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString; 
+import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter 
-
 @ToString
 public class Publicacion {
 
@@ -27,10 +28,12 @@ public class Publicacion {
     private Date fecha;
     private Boolean anonimo;
 
-      // Relaciones de agregacion y composicion 
+    // Relaciones de agregacion y composicion 
+    @ManyToOne
     Usuario autor; 
-    List <Like> like;     
 
+    @OneToMany(mappedBy = "publicacion")
+    List<Like> likes = new ArrayList<>();
 
     // Constructor con parámetros para los atributos principales
     public Publicacion(Long idAutor, String descripcion, Boolean anonimo) {
@@ -40,7 +43,12 @@ public class Publicacion {
     }
 
     // Método para cambiar el anonimato de una publicación
-    public void cambiarAnonimato(Long idPub, Boolean anonimo) {
+    public void cambiarAnonimato(Boolean anonimo) {
         this.anonimo = anonimo;
+    }
+
+    // Método para agregar un like a la publicación
+    public void agregarLike(Like nuevoLike) {
+        this.likes.add(nuevoLike);
     }
 }
