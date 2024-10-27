392c5f6f40159059b53646ce270177111acf2a93
Katheryn Guasca <katheryng2017@gmail.com>
2024-10-27 18:32:54 +0000

    Agregando metodos de la clase Like

--- proyecto/Implementacion/model/Like.java
+++ proyecto/Implementacion/model/Like.java
@@ -1,10 +1,10 @@
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,7 +14,6 @@ import lombok.ToString;
 @AllArgsConstructor
 @Getter
 @Setter
-
 @ToString
 public class Like {
 
@@ -25,7 +24,9 @@ public class Like {
     private Long idUser;
     private Long idPub;
     private Boolean anonimoLike;
-    
+
+    @ManyToOne
+    private Publicacion publicacion;
 
     // Constructor con parámetros para los atributos principales
     public Like(Long idUser, Long idPub, Boolean anonimo) {
@@ -34,9 +35,16 @@ public class Like {
         this.anonimoLike = anonimo;
     }
 
-    //Metodos de la clase
-    public void eliminarLike(Like like) {
-
+    // Método para eliminar el like
+    public void eliminarLike() {
+        if (publicacion != null) {
+            publicacion.getLikes().remove(this);
+            this.publicacion = null;
+        }
     }
 
+    // Método para cambiar el anonimato del like
+    public void cambiarAnonimatoLike(Boolean anonimo) {
+        this.anonimoLike = anonimo;
+    }
 }
